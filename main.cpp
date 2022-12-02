980d48b0d3adaa0573203c5b2e0179061b4241e7
K D Hedger <kdhedger68713@gmail.com>
2022-12-02 11:43:03 +0000

    minor bug fixes

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
  *
  * ©K. D. Hedger. Tue 31 Jul 13:14:35 BST 2018 keithdhedger@gmail.com
 
- * This file (main.cpp) is part of CliMsg.
+ * This file(main.cpp) is part of CliMsg.
 
  * CliMsg is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -25,7 +25,7 @@
 #include <getopt.h>
 
 #define APPNAME "climsg"
-#define VERSION "0.1.1"
+#define VERSION "0.1.2"
 
 #define MAX_MSG_SIZE 1024
 
@@ -36,52 +36,53 @@
 #define WAIT_MSG 0
 
 struct option long_options[] =
-	{
-		{"send",1,0,'s'},
-		{"repeat",0,0,'r'},
-		{"all",0,0,'a'},
-		{"type",1,0,'t'},
-		{"key",1,0,'k'},
-		{"wait",0,0,'w'},
-		{"flush",0,0,'f'},
-		{"version",0,0,'v'},
-		{"help",0,0,'?'},
-		{0, 0, 0, 0}
-	};
+{
+	{"send",1,0,'s'},
+	{"repeat",0,0,'r'},
+	{"all",0,0,'a'},
+	{"type",1,0,'t'},
+	{"key",1,0,'k'},
+	{"wait",0,0,'w'},
+	{"flush",0,0,'f'},
+	{"permissions",1,0,'p'},
+	{"version",0,0,'v'},
+	{"help",0,0,'?'},
+	{0, 0, 0, 0}
+};
 
 struct msgBuffer
-	{
-		long mType;
-		char mText[MAX_MSG_SIZE];
-	};
+{
+	long		mType;
+	char		mText[MAX_MSG_SIZE];
+};
 
 int			queueID;
 msgBuffer	buffer;
 int			msgType=1;
-bool		action=false;
+bool			action=false;
 int			receiveType=IPC_NOWAIT;
-bool		repeat=false;
-bool		printAll=false;
-bool		allDone=false;
-bool		flushQueue=false;
+bool			repeat=false;
+bool			printAll=false;
+bool			allDone=false;
+bool			flushQueue=false;
 int			setPerms=0660;
 
 void printHelp()
 {
 	printf("Usage: %s [OPTION] [TEXT]\n"
-		"A CLI application\n"
-		" -s, --send	Send message [TEXT] (defaults to receive)\n"
-		" -r, --repeat	Print received message and resend\n"
-		" -a, --all	Print all messages in queue\n"
-		" -f, --flush	Flush message queue quietly\n"
-		" -t, --type	Message type (defaults to 1)\n"
-		" -k, --key	Use key [INTEGER] instead of generated one\n"
-		" -w, --wait	Wait for message to arrive (blocking)\n"
-		" -p, --permissions	Set perms on queue ( default 0660 )\n"
-		" -v, --version	output version information and exit\n"
-		" -h, -?, --help	print this help\n\n"
-		"Report bugs to keithdhedger@gmail.com\n"
-		,APPNAME);
+	       "A CLI application for inter-process communication\n"
+	       " -s, --send	Send message [TEXT](defaults to receive)\n"
+	       " -r, --repeat	Print received message and resend\n"
+	       " -a, --all	Print all messages in queue\n"
+	       " -f, --flush	Flush message queue quietly\n"
+	       " -t, --type	Message type(defaults to 1)\n"
+	       " -k, --key	Use key [INTEGER] instead of generated one\n"
+	       " -w, --wait	Wait for message to arrive(blocking)\n"
+	       " -p, --permissions	Set perms on queue( default 0660 )\n"
+	       " -v, --version	output version information and exit\n"
+	       " -h, -?, --help	print this help\n\n"
+	       "Report bugs to keithdhedger@gmail.com\n"
+	       ,APPNAME);
 }
 
 void sendMsg()
@@ -115,63 +116,63 @@ int main(int argc, char **argv)
 	buffer.mText[0]=0;
 	key=0xdeadbeef;
 
-	while (1)
+	while(1)
 		{
-		int option_index = 0;
-		c = getopt_long (argc, argv, "v?hewfras:t:k:",long_options, &option_index);
-		if (c == -1)
-			break;
-
-		switch (c)
-			{
-			case 'p':
-				setPerms=atoi(optarg);
-				break;
-			case 's':
-				strcpy(buffer.mText,optarg);
-				action=true;
+			int option_index=0;
+			c=getopt_long(argc, argv, "v?hewfras:t:k:p:",long_options, &option_index);
+			if(c == -1)
 				break;
 
-			case 'r':
-				repeat=true;
-				break;
+			switch(c)
+				{
+				case 'p':
+					setPerms=atoi(optarg);
+					break;
+				case 's':
+					strcpy(buffer.mText,optarg);
+					action=true;
+					break;
 
-			case 'a':
-				printAll=true;
-				break;
+				case 'r':
+					repeat=true;
+					break;
 
-			case 'f':
-				flushQueue=true;
-				break;
+				case 'a':
+					printAll=true;
+					break;
 
-			case 't':
-				msgType=atoi(optarg);
-				break;
-		
-			case 'w':
-				receiveType=WAIT_MSG;
-				break;
+				case 'f':
+					flushQueue=true;
+					break;
 
-			case 'k':
-				key=atoi(optarg);
-				break;
+				case 't':
+					msgType=atoi(optarg);
+					break;
 
-			case 'v':
-				printf("climsg %s\n",VERSION);
-				return ALLOK;
-				break;
+				case 'w':
+					receiveType=WAIT_MSG;
+					break;
 
-			case '?':
-			case 'h':
-				printHelp();
-				return ALLOK;
-				break;
+				case 'k':
+					key=atoi(optarg);
+					break;
 
-			default:
-				fprintf(stderr,"?? Unknown argument ??\n");
-				return UNKNOWNARG;
-			break;
-			}
+				case 'v':
+					printf("climsg %s\n",VERSION);
+					return ALLOK;
+					break;
+
+				case '?':
+				case 'h':
+					printHelp();
+					return ALLOK;
+					break;
+
+				default:
+					fprintf(stderr,"?? Unknown argument ??\n");
+					return UNKNOWNARG;
+					break;
+				}
 		}
 //IPC_EXCL
 	if((queueID=msgget(key,IPC_CREAT|setPerms))==-1)
@@ -181,8 +182,8 @@ int main(int argc, char **argv)
 		}
 
 	buffer.mType=msgType;
-	
-	if (printAll==true)
+
+	if(printAll==true)
 		{
 			while(allDone==false)
 				{
@@ -191,7 +192,7 @@ int main(int argc, char **argv)
 			return(ALLOK);
 		}
 
-	if (flushQueue==true)
+	if(flushQueue==true)
 		{
 			allDone=false;
 			msgType=0;
@@ -209,11 +210,10 @@ int main(int argc, char **argv)
 	else
 		{
 			readMsg();
-			if (repeat==true)
+			if(repeat==true)
 				sendMsg();
 		}
 
 	return(ALLOK);
-	
 }
 
